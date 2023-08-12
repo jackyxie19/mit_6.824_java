3362f11ecbd787390bef4c338796aea1756ee177
jacky <554518964@qq.com>
2023-08-12 22:28:11 +0800

    Create ConcurrentHashMapPractice.java
    p

---
+++ src/javabase/collections/ConcurrentHashMapPractice.java
@@ -0,0 +1,9 @@
+package javabase.collections;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapPractice {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String,Object> concurrentHashMap = new ConcurrentHashMap();
+    }
+}
